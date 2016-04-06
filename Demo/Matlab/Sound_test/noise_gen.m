5eafc25f301f8cc04e6c677cfd53177ef1168168
Santiago Salas <santi.f.salas@gmail.com>
2016-04-06 02:58:24 -0400

    Generated Noises and Added training sets for 5 new sounds
    Generated Noises for 2 new alarms and 3 new animal sounds. Also added
    training sets for new sounds

--- Demo/Matlab/Sound_test/noise_gen.m
+++ Demo/Matlab/Sound_test/noise_gen.m
@@ -1,6 +1,6 @@
 %adding noise to sound wave files
 
-
+%{
 for n = 1:1:100
     %load file
     [y,Fs] = audioread('School_Fire_Alarm.wav');
@@ -13,7 +13,76 @@ for n = 1:1:100
 
     
 end
+
 %Other methods of adding noise
 z = imnoise( y, 'salt & pepper', 0.05);
 z = imnoise( y, 'poisson');
 z = imnoise( y, 'gaussian', Mean, Var);
+%}
+for n = 1:1:100
+    %load file
+    [y,Fs] = audioread('Warning_Siren.wav');
+    %set STD
+    STD = n /1000;
+    
+    z = y + STD*randn(size(y));
+    filename = sprintf('Warning_Siren_Vars/Warning_Siren_Noise_%d.wav',n);
+    audiowrite(filename, z, Fs)
+
+    
+end
+
+for n = 1:1:100
+    %load file
+    [y,Fs] = audioread('Smoke_Alarm.wav');
+    %set STD
+    STD = n /1000;
+    
+    z = y + STD*randn(size(y));
+    filename = sprintf('Smoke_Alarm_Vars/Smoke_Alarm_Noise_%d.wav',n);
+    audiowrite(filename, z, Fs)
+
+    
+end
+
+
+for n = 1:1:100
+    %load file
+    [y,Fs] = audioread('Dog_Kennel.wav');
+    %set STD
+    STD = n /1000;
+    
+    z = y + STD*randn(size(y));
+    filename = sprintf('Dog_Kennel_Vars/Dog_Kennel_Noise_%d.wav',n);
+    audiowrite(filename, z, Fs)
+
+    
+end
+
+
+for n = 1:1:100
+    %load file
+    [y,Fs] = audioread('Cow_Moos.wav');
+    %set STD
+    STD = n /1000;
+    
+    z = y + STD*randn(size(y));
+    filename = sprintf('Cow_Moos_Vars/Cow_Moos_Noise_%d.wav',n);
+    audiowrite(filename, z, Fs)
+
+    
+end
+
+
+for n = 1:1:100
+    %load file
+    [y,Fs] = audioread('Cat_Meows.wav');
+    %set STD
+    STD = n /1000;
+    
+    z = y + STD*randn(size(y));
+    filename = sprintf('Cat_Meows_Vars/Cat_Meows_Noise_%d.wav',n);
+    audiowrite(filename, z, Fs)
+
+    
+end
